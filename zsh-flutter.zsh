3c67a5f2bbc347c56e0cc6a278f3230eb68bda35
@slovacus <slovacus@gmail.com>
2019-06-23 16:04:16 -0500

    chore: actions for actions flutter (#6)

--- zsh-flutter.zsh
+++ zsh-flutter.zsh
@@ -8,17 +8,36 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 
-function install_flutter {
-    wget -P "$HOME/.other" -O flutter.zip https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.0.0-stable.zip && unzip -d "$HOME/.other" flutter.zip
+LIGHT_GREEN='\033[1;32m'
+CLEAR='\033[0m'
+FLUTTER_VERSION="flutter_macos_v1.5.4-hotfix.2-stable"
+
+function flutter::install {
+    echo -e "${CLEAR}${LIGHT_GREEN}Installing Flutter${CLEAR}"
+    wget -P "$HOME/.other" -O flutter.zip https://storage.googleapis.com/flutter_infra/releases/stable/macos/${FLUTTER_VERSION}.zip && unzip -d "$HOME/.other" flutter.zip
 
 }
 
-function load_flutter {
-    [[ -e "$HOME/.other/flutter/bin" ]] && export PATH="$PATH:$HOME/.other/flutter/bin"
+function flutter::dependences {
+    brew install --HEAD usbmuxd
+    brew link usbmuxd
+    brew install --HEAD libimobiledevice
+    brew install ideviceinstaller
+    brew install cocoapods
+    brew install ios-deploy
+    pod setup
 }
 
-load_flutter
+
+function flutter::load {
+    [[ -e "$HOME/.other/flutter" ]] && export FLUTTER_ROOT="$HOME/.other/flutter"
+    [[ -e "$HOME/.other/flutter/bin" ]] && export PATH="$PATH:$FLUTTER_ROOT/bin"
+    [[ -e "$HOME/.other/flutter/bin/cache/dart-sdk/" ]] && export FLUTTER_DART_SDK="$FLUTTER_ROOT/bin/cache/dart-sdk/"
+}
+
+flutter::load
 
 if (( ! $+commands[flutter] )); then
-    install_flutter
+    flutter::install
+    flutter::dependences
 fi
