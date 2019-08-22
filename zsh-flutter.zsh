391a4ace0e1fb26e5eb4e1d020dc31ff8e365065
@slovacus <slovacus@gmail.com>
2019-08-22 14:59:51 -0500

    (#14) refactor: path for sdk flutter

--- zsh-flutter.zsh
+++ zsh-flutter.zsh
@@ -10,11 +10,11 @@
 
 LIGHT_GREEN='\033[1;32m'
 CLEAR='\033[0m'
-FLUTTER_VERSION="flutter_macos_v1.5.4-hotfix.2-stable"
+FLUTTER_VERSION="flutter_macos_v1.7.8+hotfix.4-stable.zip"
 
 function flutter::install {
     echo -e "${CLEAR}${LIGHT_GREEN}Installing Flutter${CLEAR}"
-    wget -P "$HOME/.other" -O flutter.zip https://storage.googleapis.com/flutter_infra/releases/stable/macos/${FLUTTER_VERSION}.zip && unzip -d "$HOME/.other" flutter.zip
+    wget -P "$HOME/google" -O flutter.zip https://storage.googleapis.com/flutter_infra/releases/stable/macos/${FLUTTER_VERSION}.zip && unzip -d "$HOME/google" flutter.zip
 
 }
 
@@ -30,10 +30,10 @@ function flutter::dependences {
 
 
 function flutter::load {
-    [[ -e "$HOME/.other/flutter" ]] && export FLUTTER_ROOT="$HOME/.other/flutter"
-    [[ -e "$HOME/.other/flutter/bin" ]] && export PATH="$PATH:$FLUTTER_ROOT/bin"
-    [[ -e "$HOME/.other/flutter/bin/cache/dart-sdk/" ]] && export FLUTTER_DART_SDK="$FLUTTER_ROOT/bin/cache/dart-sdk/"
-    [[ -e "$HOME/.other/flutter/bin/cache/dart-sdk/bin" ]] && export PATH="$PATH:$FLUTTER_DART_SDK/bin"
+    [[ -e "$HOME/google/flutter" ]] && export FLUTTER_ROOT="$HOME/google/flutter"
+    [[ -e "$HOME/google/flutter/bin" ]] && export PATH="$PATH:$FLUTTER_ROOT/bin"
+    [[ -e "$HOME/google/flutter/bin/cache/dart-sdk/" ]] && export FLUTTER_DART_SDK="$FLUTTER_ROOT/bin/cache/dart-sdk/"
+    [[ -e "$HOME/google/flutter/bin/cache/dart-sdk/bin" ]] && export PATH="$PATH:$FLUTTER_DART_SDK/bin"
 }
 
 flutter::load
