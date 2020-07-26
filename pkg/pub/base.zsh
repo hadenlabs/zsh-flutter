ea15178dc7f33d9cb05c8e2ec6a9d722c651d1fb
@slovacus <slovacus@gmail.com>
2020-07-26 18:26:06 -0500

    chore: implement load file for pub (#45)

---
+++ pkg/pub/base.zsh
@@ -0,0 +1,21 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+export PUB_BIN_PATH="${HOME}/.pub-cache/bin"
+
+function pub::post_install {
+    message_info "Installing Dependences for Pub"
+    message_info "Installed Dependences for Pub"
+}
+
+function pub::load {
+    [ -e "${PUB_BIN_PATH}" ] && export PATH="${PATH}:${PUB_BIN_PATH}"
+}
+
+function pub::dependences {
+    if type -p pub > /dev/null; then
+        return
+    fi
+    message_info "Validating Dependences for Pub"
+    message_success "Validated Dependences for Pub"
+}
