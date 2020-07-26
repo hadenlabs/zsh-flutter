66e6498f80aef7e5b71b4bff4d22b3fb7faee7ec
@slovacus <slovacus@gmail.com>
2020-07-26 18:27:21 -0500

    chore: implement load pub and flutter (#45)

--- zsh-flutter.zsh
+++ zsh-flutter.zsh
@@ -15,9 +15,19 @@ FLUTTER_SRC_PATH=$(dirname "$0")
 # shellcheck source=/dev/null
 source "${FLUTTER_SRC_PATH}"/pkg/flutter/main.zsh
 
+# shellcheck source=/dev/null
+source "${FLUTTER_SRC_PATH}"/pkg/pub/main.zsh
+
 flutter::load
+
+pub::load
 
 if ! type -p flutter > /dev/null; then
     flutter::install
     flutter::post_install
+fi
+
+if ! type -p pub > /dev/null; then
+    pub::install
+    pub::post_install
 fi
