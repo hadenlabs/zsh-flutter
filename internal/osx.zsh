ac1b1dee5b2f61bb9608980096c9e97c730d0a8b
@slovacus <slovacus@gmail.com>
2021-03-15 15:43:59 -0500

    chore: implement install dependences android (#69)

--- internal/osx.zsh
+++ internal/osx.zsh
@@ -8,10 +8,20 @@ function flutter::internal::flutter::post_install {
     fi
 
     message_info "Installing Dependences for Flutter"
+
+    gem install cocoapods
+
+    touch "${ANDROID_FILE_REPOSITORIES}"
+
+    [ ! "$(core::exists sdkmanager)" ] && brew install --cask android-sdk
+    [ ! "$(core::exists java)" ] && brew install --cask homebrew/cask-versions/adoptopenjdk8
+
     brew install --HEAD usbmuxd
     brew link usbmuxd
     brew install --HEAD libimobiledevice
     brew install ideviceinstaller cocoapods ios-deploy
     pod setup
+
+    flutter config --android-sdk "${ANDROID_SDK_ROOT}"
     message_success "Flutter Dependences Installed"
 }
