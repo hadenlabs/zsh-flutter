e4dfe3613534a0dfdfda9918f083b4cdb4428dea
@slovacus <slovacus@gmail.com>
2021-03-15 14:23:03 -0500

    refactor: standard version stable 2.0.2 (#65)

--- internal/base.zsh
+++ internal/base.zsh
@@ -23,14 +23,6 @@ function flutter::internal::curl::install {
     message_success "Installed curl for ${FLUTTER_PACKAGE_NAME}"
 }
 
-function flutter::internal::packages::install {
-    message_info "Installing required flutter packages"
-    for package in "${FLUTTER_PACKAGES[@]}"; do
-       GO111MODULE=on go get -u -v "${package}"
-    done
-    message_success "Installed required flutter packages"
-}
-
 function flutter::internal::flutter::upgrade {
     message_warning "method not implement"
 }
