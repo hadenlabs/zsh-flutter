08dc1dae7c02a25799426158d24a208fee364b9c
Luis Mayta <luis@hadenlabs.com>
2022-05-19 09:13:22 -0500

    chore: upgrade version flutter stable (#97)

--- config/osx.zsh
+++ config/osx.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export FLUTTER_FILE="macos/flutter_macos_${FLUTTER_VERSION}.zip"
+export FLUTTER_FILE="macos/flutter_macos_arm64_${FLUTTER_VERSION}.zip"
